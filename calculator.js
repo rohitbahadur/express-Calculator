224d11db1ada8b11955ea98d132c0734ce15be88
rbahadur <rohit.bahadur@diva-e.com>
2022-11-19 23:26:23 +0100

    update var name

--- calculator.js
+++ calculator.js
@@ -10,8 +10,8 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-	var number1 = Number(req.body.num1);
-	var number2 = Number(req.body.num2);
+	var number1 = Number(req.body.n1);
+	var number2 = Number(req.body.n2);
 
 	var result = number1 + number2;
 
@@ -20,7 +20,7 @@ app.post("/", function (req, res) {
 })
 
 
-//bmi calculator 
+//bmi calculator (for this we need to change the url routing to /bmicalculator)
 
 app.get("/bmicalculator", function (req, res) {
 	res.sendFile(__dirname + "/bmiCalculator.html")
